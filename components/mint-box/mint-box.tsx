42b051f377e74e131d52414c0e932f1f88253038
SuarezLDamian <suarezldamian@gmail.com>
2022-03-02 17:30:16 -0300

    Components created, unnecessary files deleted.

---
+++ components/mint-box/mint-box.tsx
@@ -0,0 +1,45 @@
+import { Box, Image, Button, Text, Center, VStack, StackDivider } from "@chakra-ui/react";
+import {
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
+  } from '@chakra-ui/react';
+
+const MintBox = () => {
+    return (
+        <Box maxW='md' borderWidth='3px' borderRadius='lg' mt='2rem'>
+            <VStack spacing={1}>
+                <Center>
+                    <Box padding={'2rem'}>
+                        <Image src='/sample-image.png' alt='Sample Epuhoria Club' />
+                    </Box>
+                </Center>
+                <Center>
+                    <Text fontSize='lg' >0 / 4420</Text>
+                </Center>
+                <Center>
+                    <Text fontSize='lg' >0.01 ETH</Text>
+                </Center>
+                <Box padding={'2rem'}>
+                    <Text fontSize='md' >Enter quantity</Text>
+                    <NumberInput defaultValue={1} min={1} max={10}>
+                        <NumberInputField />
+                        <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                        </NumberInputStepper>
+                    </NumberInput>
+                    <Center>
+                        <Button size='lg' colorScheme='green' mt='1rem'>
+                            Mint
+                        </Button>
+                    </Center>
+                </Box>
+            </VStack>
+        </Box>
+    )
+}
+
+export default MintBox;

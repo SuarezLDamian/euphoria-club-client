75340c89b3d9b1179ba94c748c84ad5366ea5463
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-03-07 20:34:45 -0300

    Web3 integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
+import { getLibrary } from '../config/web3/index'
+import { Web3ReactProvider } from '@web3-react/core'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Web3ReactProvider>
   )
 }
 

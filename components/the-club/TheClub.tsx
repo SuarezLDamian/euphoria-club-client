de732f4b9473e3a3fe0bc97ae12f60b40fe730af
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-03-09 19:24:30 -0300

    Centered text.

--- components/the-club/TheClub.tsx
+++ components/the-club/TheClub.tsx
@@ -10,20 +10,20 @@ const TheClub = () => {
                 </Heading>
             </Center>
             <Center>
-                <Stack spacing={12} margin={'4rem'}>                
-                    <Text fontSize='2xl' style={{color: 'white'}}>Si eres fan de la famosa serie Euphoria como nosotros, no pierdas la oportunidad de formar parte de la comunidad y acuñar un nuevo integrante, o si eres de esas personas que tienen suerte, ¡Acuñar un integrante oficial del Cast con sus rasgos oficiales!</Text>
+                <Stack spacing={12} margin='4rem'>                
+                    <Text fontSize='2xl' align='center' style={{color: 'white'}}>Si eres fan de la famosa serie Euphoria como nosotros, no pierdas la oportunidad de formar parte de la comunidad y acuñar un nuevo integrante, o si eres de esas personas que tienen suerte, ¡Acuñar un integrante oficial del Cast con sus rasgos oficiales!</Text>
                     <Center height='2rem'>
                         <Divider orientation='vertical' />
                     </Center>
-                    <Text fontSize='2xl' style={{color: 'white'}}>No existen dos personajes similares, cada uno cuenta con un conjunto de rasgos diferentes. Los Euphoria Club se generaron a partir de un contrato inteligente utilizando mas de 60 rasgos diferentes dibujados a mano. Los Euphoria Club se acuñan, almacenan y comercializan en la cadena de bloques de Ethereum.</Text>
+                    <Text fontSize='2xl' align='center' style={{color: 'white'}}>No existen dos personajes similares, cada uno cuenta con un conjunto de rasgos diferentes. Los Euphoria Club se generaron a partir de un contrato inteligente utilizando mas de 60 rasgos diferentes dibujados a mano. Los Euphoria Club se acuñan, almacenan y comercializan en la cadena de bloques de Ethereum.</Text>
                     <Center height='2rem'>
                         <Divider orientation='vertical' />
                     </Center>
-                    <Text fontSize='2xl' style={{color: 'white'}}>En euphoria club odiamos la discriminación, queremos que todos se sientan cómodos. No importa raza, sexo, ni físico. Lo importante es tu esencia interior.</Text>
+                    <Text fontSize='2xl' align='center' style={{color: 'white'}}>En euphoria club odiamos la discriminación, queremos que todos se sientan cómodos. No importa raza, sexo, ni físico. Lo importante es tu esencia interior.</Text>
                     <Center height='2rem'>
                         <Divider orientation='vertical' />
                     </Center>
-                    <Text fontSize='2xl' style={{color: 'white'}}>Una vez comenzada la 3er Temporada de la Serie, todos los que holdeen sus Euphoria Club NFT, podran acceder a un minteo gratuito de la coleccion N°2.</Text>
+                    <Text fontSize='2xl' align='center' style={{color: 'white'}}>Una vez comenzada la 3er Temporada de la Serie, todos los que holdeen sus Euphoria Club NFT, podran acceder a un minteo gratuito de la coleccion N°2.</Text>
                 </Stack>
             </Center>
             

1196bdc2d0caa03e373311570535391276be933e
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-03-12 12:27:59 -0300

    Accordion styling, replaced developer avatar.

--- components/faq-accordion/FaqAccordion.tsx
+++ components/faq-accordion/FaqAccordion.tsx
@@ -30,7 +30,7 @@ const FaqAccordion = () => {
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
-                    <AccordionPanel pb={4}>
+                    <AccordionPanel p={'2rem'} style={{color: 'white'}}>
                     Euphoria Clubbers are randomly generated, non-fungible tokens, also known as an &quot;NFT.&quot; There is 4420 different Euphoria Clubbers and each Character is completely unique. No two Euphoria Clubbers are the same. The Euphoria Clubbers were generated from a smart contract using over 150 different hand drawn traits. Euphoria Clubbers are minted, stored, and traded on the Ethereum blockchain.
                     </AccordionPanel>
                 </AccordionItem>
@@ -44,7 +44,7 @@ const FaqAccordion = () => {
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
-                    <AccordionPanel pb={4}>
+                    <AccordionPanel p={'2rem'} style={{color: 'white'}}>
                     You can mint an Euphoria Clubber from this website or purchase it from our verified collection on the OpenSea marketplace.
                     </AccordionPanel>
                 </AccordionItem>
@@ -58,7 +58,7 @@ const FaqAccordion = () => {
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
-                    <AccordionPanel pb={4}>
+                    <AccordionPanel p={'2rem'} style={{color: 'white'}}>
                     The launch date is tbd.
                     </AccordionPanel>
                 </AccordionItem>
@@ -72,7 +72,7 @@ const FaqAccordion = () => {
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
-                    <AccordionPanel pb={4}>
+                    <AccordionPanel p={'2rem'} style={{color: 'white'}}>
                     4,420 Euphoria Clubbers.
                     </AccordionPanel>
                 </AccordionItem>
@@ -86,7 +86,7 @@ const FaqAccordion = () => {
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
-                    <AccordionPanel pb={4}>
+                    <AccordionPanel p={'2rem'} style={{color: 'white'}}>
                     Every Euphoria Clubber has unique traits/attributes, but some traits are more rare than others. For example, 396 Euphoria Clubbers have a Pink Background, but only 22 have Tears. You can see the traits for an Euphoria Clubber under &quot;properties&quot; on OpenSea.
                     </AccordionPanel>
                 </AccordionItem>

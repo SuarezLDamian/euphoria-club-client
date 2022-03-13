2d40efa450e8e8695b7d2dce641ee8cb12f6840f
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-03-13 06:17:47 -0300

    Faq styling.

--- components/faq-accordion/FaqAccordion.tsx
+++ components/faq-accordion/FaqAccordion.tsx
@@ -7,7 +7,8 @@ import {
     Box,
     Heading,
     Center, 
-    Container
+    Container,
+    Text
   } from '@chakra-ui/react'
 import Image from 'next/image'
 
@@ -28,70 +29,90 @@ const FaqAccordion = () => {
                 <AccordionItem>
                     <h2>
                     <AccordionButton>
-                        <Box flex='1' textAlign='left' style={{color: 'white'}}>
-                        1. WHAT IS EUPHORIA CLUB ?
+                        <Box flex='1' textAlign='left' p={'1rem'} style={{color: 'white'}}>
+                            <Text fontSize='xl' style={{color: 'white'}}>
+                                1. WHAT IS EUPHORIA CLUB ?
+                            </Text>
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
                     <AccordionPanel p={'2rem'} style={{color: 'white'}}>
-                    Euphoria Clubbers are randomly generated, non-fungible tokens, also known as an &quot;NFT.&quot; There is 4420 different Euphoria Clubbers and each Character is completely unique. No two Euphoria Clubbers are the same. The Euphoria Clubbers were generated from a smart contract using over 150 different hand drawn traits. Euphoria Clubbers are minted, stored, and traded on the Ethereum blockchain.
+                        <Text fontSize='lg' style={{color: 'white'}}>
+                        Euphoria Clubbers are randomly generated, non-fungible tokens, also known as an &quot;NFT.&quot; There is 4420 different Euphoria Clubbers and each Character is completely unique. No two Euphoria Clubbers are the same. The Euphoria Clubbers were generated from a smart contract using over 150 different hand drawn traits. Euphoria Clubbers are minted, stored, and traded on the Ethereum blockchain.
+                        </Text>
                     </AccordionPanel>
                 </AccordionItem>
 
                 <AccordionItem>
                     <h2>
                     <AccordionButton>
-                        <Box flex='1' textAlign='left' style={{color: 'white'}}>
-                        2. HOW DO I PURCHASE A CLUBBER ?
+                        <Box flex='1' textAlign='left' p={'1rem'} style={{color: 'white'}}>
+                            <Text fontSize='xl' style={{color: 'white'}}>
+                            2. HOW DO I PURCHASE A CLUBBER ?
+                            </Text>
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
                     <AccordionPanel p={'2rem'} style={{color: 'white'}}>
-                    You can mint an Euphoria Clubber from this website or purchase it from our verified collection on the OpenSea marketplace.
+                        <Text fontSize='lg' style={{color: 'white'}}>
+                            You can mint an Euphoria Clubber from this website or purchase it from our verified collection on the OpenSea marketplace.
+                        </Text>
                     </AccordionPanel>
                 </AccordionItem>
 
                 <AccordionItem>
                     <h2>
                     <AccordionButton>
-                        <Box flex='1' textAlign='left' style={{color: 'white'}}>
-                        3. WHEN WILL EUPHORIA CLUBBERS LAUNCH ?
+                        <Box flex='1' textAlign='left' p={'1rem'} style={{color: 'white'}}>
+                            <Text fontSize='xl' style={{color: 'white'}}>
+                            3. WHEN WILL EUPHORIA CLUBBERS LAUNCH ?
+                            </Text>
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
                     <AccordionPanel p={'2rem'} style={{color: 'white'}}>
-                    The launch date is tbd.
+                        <Text fontSize='lg' style={{color: 'white'}}>
+                            The launch date is tbd.
+                        </Text>
                     </AccordionPanel>
                 </AccordionItem>
 
                 <AccordionItem>
                     <h2>
                     <AccordionButton>
-                        <Box flex='1' textAlign='left' style={{color: 'white'}}>
-                        4. HOW MANY EUPHORIA CLUBBERS EXIST ?
+                        <Box flex='1' textAlign='left' p={'1rem'} style={{color: 'white'}}>
+                            <Text fontSize='xl' style={{color: 'white'}}>
+                                4. HOW MANY EUPHORIA CLUBBERS EXIST ?
+                            </Text>
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
                     <AccordionPanel p={'2rem'} style={{color: 'white'}}>
-                    4,420 Euphoria Clubbers.
+                        <Text fontSize='lg' style={{color: 'white'}}>
+                            4,420 Euphoria Clubbers.
+                        </Text>
                     </AccordionPanel>
                 </AccordionItem>
                 
                 <AccordionItem>
                     <h2>
                     <AccordionButton>
-                        <Box flex='1' textAlign='left'style={{color: 'white'}}>
-                        5. ARE SOME EUPHORIA CLUBBERS MORE RARE THAN OTHERS ?
+                        <Box flex='1' textAlign='left' p={'1rem'} style={{color: 'white'}}>
+                            <Text fontSize='xl' style={{color: 'white'}}>
+                                5. ARE SOME EUPHORIA CLUBBERS MORE RARE THAN OTHERS ?
+                            </Text>
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                     </h2>
                     <AccordionPanel p={'2rem'} style={{color: 'white'}}>
-                    Every Euphoria Clubber has unique traits/attributes, but some traits are more rare than others. For example, 396 Euphoria Clubbers have a Pink Background, but only 22 have Tears. You can see the traits for an Euphoria Clubber under &quot;properties&quot; on OpenSea.
+                        <Text fontSize='lg' style={{color: 'white'}}>
+                        Every Euphoria Clubber has unique traits/attributes, but some traits are more rare than others. For example, 396 Euphoria Clubbers have a Pink Background, but only 22 have Tears. You can see the traits for an Euphoria Clubber under &quot;properties&quot; on OpenSea.
+                        </Text>
                     </AccordionPanel>
                 </AccordionItem>
             </Accordion>

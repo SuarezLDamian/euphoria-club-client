bf2918f34cc78b6dba69171a308860a26b550bdd
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-05-22 19:58:17 -0300

    Added animations to Hero, created separated component for Social Button.

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react';
-import { Box, chakra, Heading, Center, Flex, VStack, Stack, Text, VisuallyHidden, useColorModeValue, Container } from '@chakra-ui/react'
+import { Box, Heading, Center, VStack, Stack, Text, Container } from '@chakra-ui/react'
 import { FaInstagram, FaTwitter, FaDiscord } from 'react-icons/fa';
-import Background from "/public/hero-background.svg"
+import { motion } from 'framer-motion';
 import MintBox from '../mint-box/mint-box'
+import SocialButton from '../social-button/SocialButton'
+import Background from "/public/hero-background.svg"
 import Image from 'next/image';
 
 const Hero = () => {
@@ -10,73 +12,47 @@ const Hero = () => {
     return (
         <>
         {/* <Image src={'/hero-backgroundd.svg'} alt='hero-background' layout='fill'></Image> */}
-        <Box backgroundImage={'/hero-background.svg'} style={{ padding: '6rem'}}>
-            <Container maxW={'6xl'}>
-                <Stack direction={['column', 'row']} spacing={'4rem'}>
-                    <Center>
-                        <MintBox/>
-                    </Center>
-                    <VStack spacing={'3rem'}>
-                        <Heading as='h1' size='4xl' style={{color: 'white'}}>
-                            WELCOME TO EUPHORIA CLUB!
-                        </Heading>
+            <Box backgroundImage={'/hero-background.svg'} style={{ padding: '6rem'}}>
+                <Container maxW={'6xl'}>
+                    <Stack direction={['column', 'row']} spacing={'4rem'}>
+                        <Center>
+                            <MintBox/>
+                        </Center>
+                        <VStack spacing={'3rem'}>
+                            <Heading as={motion.h1} size='4xl' style={{color: 'white'}}
+                            animate={{
+                                y: [200, 0],
+                            }}>
+                                WELCOME TO EUPHORIA CLUB!
+                            </Heading>
 
-                        <Text fontSize='3xl' style={{color: 'white'}}>
-                            Euphoria Club is a collection of 4,420 randomly 
-                            generated NFTs minted on the Ethereum blockchain, 
-                            inspired by the acclaimed Euphoria Series.
-                        </Text>
-                    </VStack>
-                </Stack>
-                <Center>            
-                    <Stack marginTop={'4rem'} direction={'row'} spacing={6}>
-                        <SocialButton label={'Twitter'} href={'https://twitter.com/euphorianftclub'}>
-                            <FaTwitter style={{color: 'white'}}/>
-                        </SocialButton>
-                        <SocialButton label={'Discord'} href={'https://discord.gg/pEmvhJ5e8H'}>
-                            <FaDiscord style={{color: 'white'}}/>
-                        </SocialButton>
-                        <SocialButton label={'Instagram'} href={'https://www.instagram.com/euphoriaclubnfts/'}>
-                            <FaInstagram style={{color: 'white'}}/>
-                        </SocialButton>
+                            <Text as={motion.h2} fontSize='3xl' style={{color: 'white'}}
+                            animate={{
+                                y: [200, 0],
+                            }}>
+                                Euphoria Club is a collection of 4,420 randomly 
+                                generated NFTs minted on the Ethereum blockchain, 
+                                inspired by the acclaimed Euphoria Series.
+                            </Text>
+                        </VStack>
                     </Stack>
-                </Center>
-            </Container>
-        </Box>
+                    <Center>            
+                        <Stack marginTop={'4rem'} direction={'row'} spacing={6}>
+                            <SocialButton label={'Twitter'} href={'https://twitter.com/euphorianftclub'}>
+                                <FaTwitter style={{color: 'white'}}/>
+                            </SocialButton>
+                            <SocialButton label={'Discord'} href={'https://discord.gg/pEmvhJ5e8H'}>
+                                <FaDiscord style={{color: 'white'}}/>
+                            </SocialButton>
+                            <SocialButton label={'Instagram'} href={'https://www.instagram.com/euphoriaclubnfts/'}>
+                                <FaInstagram style={{color: 'white'}}/>
+                            </SocialButton>
+                        </Stack>
+                    </Center>
+                </Container>
+            </Box>
         </>
     )
 }
-
-const SocialButton = ({
-    children,
-    label,
-    href,
-  }: {
-    children: ReactNode;
-    label: string;
-    href: string;
-  }) => {
-    return (
-      <chakra.button
-        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-        rounded={'full'}
-        w={'3rem'}
-        h={'3rem'}
-        cursor={'pointer'}
-        as={'a'}
-        href={href}
-        display={'inline-flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        transition={'background 0.3s ease'}
-        target={'_blank'}
-        _hover={{
-          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-        }}>
-        <VisuallyHidden>{label}</VisuallyHidden>
-        {children}
-      </chakra.button>
-    );
-  };
 
 export default Hero
---
+++ components/social-button/SocialButton.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react';
+import { chakra, VisuallyHidden, useColorModeValue } from '@chakra-ui/react'
+
+
+const SocialButton = ({
+    children,
+    label,
+    href,
+  }: {
+    children: ReactNode;
+    label: string;
+    href: string;
+  }) => {
+    return (
+      <chakra.button
+        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+        rounded={'full'}
+        w={'3rem'}
+        h={'3rem'}
+        cursor={'pointer'}
+        as={'a'}
+        href={href}
+        display={'inline-flex'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        transition={'background 0.3s ease'}
+        target={'_blank'}
+        _hover={{
+          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+        }}>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        {children}
+      </chakra.button>
+    );
+  };
+
+export default SocialButton;
